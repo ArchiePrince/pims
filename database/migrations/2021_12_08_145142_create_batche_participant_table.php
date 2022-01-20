6cce3807995c3a659b19f49155e0684ceb5741fd
Prince Acheampong <acheampongprince77@gmail.com>
2022-01-20 11:14:14 +0000

    Lastest Update

---
+++ database/migrations/2021_12_08_145142_create_batche_participant_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBatcheParticipantTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('batche_participant', function (Blueprint $table) {
+            $table->unsignedInteger('bid');
+            $table->unsignedInteger('pid');
+            $table->foreign('bid')->references('bid')->on('batches')->onUpdate('NO ACTION')->onDelete('CASCADE');
+            $table->foreign('pid')->references('pid')->on('participants')->onUpdate('NO ACTION')->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('batche_participant');
+    }
+}
